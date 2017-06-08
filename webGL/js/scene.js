cb70c98683789fb10f9f7cffa6957ec548105147
Elna Cabot Paredes <ls30917@salleurl.edu>
2017-06-08 19:15:42 +0200

    Shooter

--- webGL/js/scene.js
+++ webGL/js/scene.js
@@ -1,245 +1,146 @@
-var renderer;
-var scene;
-var camera;
-var mouseX = 0;
-var mouseY = 0;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-var moveFront = false;
-var moveLeft = false;
-var moveRight = false;
-var moveBack = false;
-var x = 0;
-var y = 0;
-var z = 100;
-var c_x = 0;
-var c_y = 0;
-var c_z = 500;
-var lookAt = new THREE.Vector3(0,0,100);
+
+var scene, camera, renderer, mesh, clock;
+var meshFloor, ambientLight, light;
+
+var crate, crateTexture, crateNormalMap, crateBumpMap;
+var modelArma;
 var keyboard = {};
-var player = { height:1.8, speed:10, turnSpeed:Math.PI*10 };
+var player = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02, canShoot:0 };
+var USE_WIREFRAME = false;
+
+var loadingScreen = {
+    scene: new THREE.Scene(),
+    camera: new THREE.PerspectiveCamera(90, 1280/720, 0.1, 100),
+    box: new THREE.Mesh(
+        new THREE.BoxGeometry(0.5,0.5,0.5),
+        new THREE.MeshBasicMaterial({ color:0x4444ff })
+    )
+};
+var loadingManager = null;
+var RESOURCES_LOADED = false;
+var index = 0;
+
+// Models index
+var models = {
+    uzi: {
+        obj:"models/uziGold.obj",
+        mtl:"models/uziGold.mtl",
+        mesh: null,
+        castShadow:false
+    },
+    uzi2: {
+        obj:"models/uziLong.obj",
+        mtl:"models/uziLong.mtl",
+        mesh: null,
+        castShadow:false
+    },
+    machine: {
+        obj:"models/machinegunLauncher.obj",
+        mtl:"models/machinegunLauncher.mtl",
+        mesh: null,
+        castShadow:false
+    }
+
+};
+
+// Meshes index
 var meshes = {};
 
-
+// Bullets array
+var bullets = [];
 
 function init(){
     scene = new THREE.Scene();
-    createCamera();
-    //createFigure();
-    createHaunter();
-    createEscenari();
-    //backgroundMusic();
-    //createEnviroment();
-    createRenderer();
-    createLight();
-    document.body.appendChild(renderer.domElement);
-    render();
+    camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000);
+    clock = new THREE.Clock();
 
-}
+    loadingScreen.box.position.set(0,0,5);
+    loadingScreen.camera.lookAt(loadingScreen.box.position);
+    loadingScreen.scene.add(loadingScreen.box);
 
-function backgroundMusic(){
-    var listener = new THREE.AudioListener();
-    var audioLoader = new THREE.AudioLoader();
-    var sound2 = new THREE.PositionalAudio( listener );
-    audioLoader.load( 'assets/Cancion del titere.mp3', function( buffer ) {
-        sound2.setBuffer( buffer );
-        sound2.setRefDistance( 20 );
-        sound2.play();
-    });
-}
-
-
-/*window.addEventListener("keydown", function(e){
- });*/
-
-function keyDown(event){
-    keyboard[event.keyCode] = true;
-}
-
-function keyUp(event){
-    keyboard[event.keyCode] = false;
-}
+    loadingManager = new THREE.LoadingManager();
+    loadingManager.onProgress = function(item, loaded, total){
+        console.log(item, loaded, total);
+    };
+    loadingManager.onLoad = function(){
+        console.log("loaded all resources");
+        RESOURCES_LOADED = true;
+        modelArma = models.uzi.mesh.clone();
+        onResourcesLoaded();
+    };
 
 
 
-function moveCharacter(){
-    var time = Date.now() * 0.0005;
-    if(keyboard[87]){ // W key
-        camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
-        camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
-        meshes['character'].position.set(
-            camera.position.x - Math.sin(camera.rotation.y + Math.PI/6) * 0.75,
-            camera.position.y - 0.5 + Math.sin(time*4 + camera.position.x + camera.position.z)*0.01,
-            camera.position.z + Math.cos(camera.rotation.y + Math.PI/6) * 0.75 -400
-        );
-        meshes['character'].rotation.set(
-            camera.rotation.x,
-            camera.rotation.y - Math.PI,
-            camera.rotation.z
-        );
-    }
-    if(keyboard[83]){ // S key
-        camera.position.x += Math.sin(camera.rotation.y) * player.speed;
-        camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
-        meshes['character'].position.set(
-            camera.position.x - Math.sin(camera.rotation.y + Math.PI/6) * 0.75,
-            camera.position.y - 0.5 + Math.sin(time*4 + camera.position.x + camera.position.z)*0.01,
-            camera.position.z + Math.cos(camera.rotation.y + Math.PI/6) * 0.75-400
-        );
-        meshes['character'].rotation.set(
-            camera.rotation.x,
-            camera.rotation.y - Math.PI,
-            camera.rotation.z
-        );
-    }
-    if(keyboard[65]){ // A key
-        // Redirect motion by 90 degrees
-        camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
-        camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
-        meshes['character'].position.set(
-            camera.position.x - Math.sin(camera.rotation.y + Math.PI/6) * 0.75,
-            camera.position.y - 0.5 + Math.sin(time*4 + camera.position.x + camera.position.z)*0.01,
-            camera.position.z + Math.cos(camera.rotation.y + Math.PI/6) * 0.75-400
-        );
-        meshes['character'].rotation.set(
-            camera.rotation.x,
-            camera.rotation.y - Math.PI,
-            camera.rotation.z
-        );
-    }
-    if(keyboard[68]){ // D key
-        camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
-        camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
-        meshes['character'].position.set(
-            camera.position.x - Math.sin(camera.rotation.y + Math.PI/6) * 0.75,
-            camera.position.y - 0.5 + Math.sin(time*4 + camera.position.x + camera.position.z)*0.01,
-            camera.position.z + Math.cos(camera.rotation.y + Math.PI/6) * 0.75-400
-        );
-        meshes['character'].rotation.set(
-            camera.rotation.x,
-            camera.rotation.y - Math.PI,
-            camera.rotation.z
-        );
-    }
-
-    // Keyboard turn inputs
-    if(keyboard[37]){ // left arrow key
-        camera.rotation.y -= player.turnSpeed;
-        meshes['character'].position.set(
-            camera.position.x - Math.sin(camera.rotation.y + Math.PI/6) * 0.75,
-            camera.position.y - 0.5 + Math.sin(time*4 + camera.position.x + camera.position.z)*0.01,
-            camera.position.z + Math.cos(camera.rotation.y + Math.PI/6) * 0.75 -400
-        );
-        meshes['character'].rotation.set(
-            camera.rotation.x,
-            camera.rotation.y - Math.PI,
-            camera.rotation.z
-        );
-    }
-    if(keyboard[39]){ // right arrow key
-        camera.rotation.y += player.turnSpeed;
-        meshes['character'].position.set(
-            camera.position.x - Math.sin(camera.rotation.y + Math.PI/6) * 0.75,
-            camera.position.y - 0.5 + Math.sin(time*4 + camera.position.x + camera.position.z)*0.01,
-            camera.position.z + Math.cos(camera.rotation.y + Math.PI/6) * 0.75 -400
-        );
-        meshes['character'].rotation.set(
-            camera.rotation.x,
-            camera.rotation.y - Math.PI,
-            camera.rotation.z
-        );
-    }
+    meshFloor = new THREE.Mesh(
+        new THREE.PlaneGeometry(20,20, 10,10),
+        new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME})
+    );
+    meshFloor.rotation.x -= Math.PI / 2;
+    meshFloor.receiveShadow = true;
+    scene.add(meshFloor);
 
 
-
-}
-
-
-
-function render(){
-    cameraControl.update();
-    moveCharacter();
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
-}
-
-function createRenderer(){
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0x000000, 1.0);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
-}
-
-function createCamera(){
-    camera = new THREE.PerspectiveCamera(
-        45,
-        window.innerWidth/ window.innerHeight,
-        0.1, 1000);
-
-    cameraControl = new THREE.OrbitControls(camera);
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 500;
-    camera.lookAt(lookAt);
-
-}
-
-
-function createLight(){
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff,1);
-    directionalLight.position.set(0, 500, 500);
-    directionalLight.name = 'directional';
-    scene.add(directionalLight);
-
-    var ambientLight = new THREE.AmbientLight(0x111111);
+    ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
     scene.add(ambientLight);
 
+    light = new THREE.PointLight(0xffffff, 0.8, 18);
+    light.position.set(-3,6,-3);
+    light.castShadow = true;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 25;
+    scene.add(light);
+
+
+
+    for( var _key in models ){
+        (function(key){
+
+            var mtlLoader = new THREE.MTLLoader(loadingManager);
+            mtlLoader.load(models[key].mtl, function(materials){
+                materials.preload();
+
+                var objLoader = new THREE.OBJLoader(loadingManager);
+
+                objLoader.setMaterials(materials);
+                objLoader.load(models[key].obj, function(mesh){
+
+                    mesh.traverse(function(node){
+                        if( node instanceof THREE.Mesh ){
+                            if('castShadow' in models[key])
+                                node.castShadow = models[key].castShadow;
+                            else
+                                node.castShadow = true;
+
+                            if('receiveShadow' in models[key])
+                                node.receiveShadow = models[key].receiveShadow;
+                            else
+                                node.receiveShadow = true;
+                        }
+                    });
+                    models[key].mesh = mesh;
+
+                });
+            });
+
+        })(_key);
+    }
+
+    camera.position.set(0, player.height, -5);
+    camera.lookAt(new THREE.Vector3(0,player.height,0));
+
+    createHaunter();
+    createEnviroment();
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(1280, 720);
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.BasicShadowMap;
+
+    document.body.appendChild(renderer.domElement);
+
+    animate();
 }
-
-function createFigureMaterial(){
-
-    var earthTexture = new THREE.Texture();
-    var loader = new THREE.ImageLoader();
-    loader.load('assets/lee_diffuse.jpg' , function(image) {
-        earthTexture.image = image;
-        earthTexture.needsUpdate = true;
-    });
-    var earthMaterial = new THREE.MeshPhongMaterial();
-    earthMaterial.map = earthTexture;
-
-    var normalMap = new THREE.Texture();
-    loader.load('assets/lee_normal_tangent.jpg', function(image){
-        normalMap.image = image;
-        normalMap.needsUpdate = true;
-    });
-    earthMaterial.normalMap = normalMap;
-    earthMaterial.normalScale = new THREE.Vector2(1.0,1.0);
-
-    var specularMap = new THREE.Texture();
-    loader.load('assets/lee_spec.jpg', function(image){
-        specularMap.image = image;
-        specularMap.needsUpdate = true;
-    });
-    earthMaterial.specularMap = specularMap;
-    earthMaterial.specular = new THREE.Color(0x262626);
-
-
-    return earthMaterial;
-}
-
-
-
-
-function createEnviroment(){
-    var envGeometry = new THREE.SphereGeometry(500, 500, 500);
-    var envMaterial = new THREE.MeshBasicMaterial();
-    envMaterial.map = THREE.ImageUtils.loadTexture('assets/galaxy_starfield.png');
-    envMaterial.side = THREE.BackSide;
-    var envMesh = new THREE.Mesh(envGeometry, envMaterial);
-    scene.add(envMesh);
-}
-
 
 function createHaunter(){
     var material = new THREE.MeshPhongMaterial();
@@ -254,8 +155,8 @@ function createHaunter(){
             }
         });
         object.name = 'character';
-        object.position.set(0,0,100);
-        object.scale.set(0.25,0.25,0.25);
+        object.position.set(0,1,0);
+        object.scale.set(0.0025,0.0025,0.0025);
         object.rotation.y = 3.25;
         meshes['character'] = object;
         scene.add(object);
@@ -274,16 +175,157 @@ function createHaunterMaterial(){
     return earthMaterial;
 }
 
-function createEscenari(){
+
+// Runs when all resources are loaded
+function onResourcesLoaded(){
+    // player weapon
+    meshes["arma"] = modelArma;
+    meshes["arma"].position.set(0,2,0);
+    meshes["arma"].scale.set(10,10,10);
+    scene.add(meshes["arma"]);
+}
+
+function animate(){
+
+    // Play the loading screen until resources are loaded.
+    if( RESOURCES_LOADED == false ){
+        requestAnimationFrame(animate);
+
+        loadingScreen.box.position.x -= 0.05;
+        if( loadingScreen.box.position.x < -10 ) loadingScreen.box.position.x = 10;
+        loadingScreen.box.position.y = Math.sin(loadingScreen.box.position.x);
+
+        renderer.render(loadingScreen.scene, loadingScreen.camera);
+        return;
+    }
+
+    requestAnimationFrame(animate);
+
+    var time = Date.now() * 0.0005;
+    var delta = clock.getDelta();
+
+    for(var index=0; index<bullets.length; index+=1){
+        if( bullets[index] === undefined ) continue;
+        if( bullets[index].alive == false ){
+            bullets.splice(index,1);
+            continue;
+        }
+
+        bullets[index].position.add(bullets[index].velocity);
+    }
+
+    if(keyboard[87]){ // W
+        camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+        camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+    }
+    if(keyboard[83]){ // S
+        camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+    }
+    if(keyboard[65]){ // A
+        camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
+    }
+    if(keyboard[68]){ // D
+        camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
+    }
+
+    if(keyboard[37]){ // left arrow key
+        camera.rotation.y -= player.turnSpeed;
+    }
+    if(keyboard[39]){ // right arrow key
+        camera.rotation.y += player.turnSpeed;
+    }
+
+    // shoot a bullet
+    if(keyboard[32] && player.canShoot <= 0){ // spacebar key
+        // creates a bullet as a Mesh object
+        var bullet = new THREE.Mesh(
+            new THREE.SphereGeometry(0.05,8,8),
+            new THREE.MeshBasicMaterial({color:0xffffff})
+        );
+
+        // position the bullet to come from the player's weapon
+        bullet.position.set(
+            meshes["arma"].position.x,
+            meshes["arma"].position.y + 0.15,
+            meshes["arma"].position.z
+        );
+
+        // set the velocity of the bullet
+        bullet.velocity = new THREE.Vector3(
+            -Math.sin(camera.rotation.y),
+            0,
+            Math.cos(camera.rotation.y)
+        );
+
+        bullet.alive = true;
+        setTimeout(function(){
+            bullet.alive = false;
+            scene.remove(bullet);
+        }, 1000);
+
+        bullets.push(bullet);
+        scene.add(bullet);
+        player.canShoot = 10;
+    }
+
+    if(keyboard[49]) { //up arrow
+        modelArma = models.uzi.mesh.clone();
+        scene.remove(meshes["arma"]);
+        onResourcesLoaded();
+    }
+    if(keyboard[50]) { //up arrow
+        modelArma = models.uzi2.mesh.clone();
+        scene.remove(meshes["arma"]);
+        onResourcesLoaded();
+    }
+    if(keyboard[51]) { //up arrow
+        modelArma = models.machine.mesh.clone();
+        scene.remove(meshes["arma"]);
+        onResourcesLoaded();
+    }
+
+
+    if(player.canShoot > 0) player.canShoot -= 1;
+
+    // position the gun in front of the camera
+    meshes["arma"].position.set(
+        camera.position.x - Math.sin(camera.rotation.y + Math.PI/6) * 0.75,
+        camera.position.y - 0.5 + Math.sin(time*4 + camera.position.x + camera.position.z)*0.01,
+        camera.position.z + Math.cos(camera.rotation.y + Math.PI/6) * 0.75
+    );
+    meshes["arma"].rotation.set(
+        camera.rotation.x,
+        camera.rotation.y - Math.PI,
+        camera.rotation.z
+    );
+
+    renderer.render(scene, camera);
+}
+
+function createEnviroment(){
+    var envGeometry = new THREE.SphereGeometry(500, 500, 500);
+    var envMaterial = new THREE.MeshBasicMaterial();
+    envMaterial.map = THREE.ImageUtils.loadTexture('assets/sky.jpg');
+    envMaterial.side = THREE.BackSide;
+    var envMesh = new THREE.Mesh(envGeometry, envMaterial);
+    scene.add(envMesh);
 
 
 }
 
 
+function keyDown(event){
+    keyboard[event.keyCode] = true;
+}
 
-
-
-init();
+function keyUp(event){
+    keyboard[event.keyCode] = false;
+}
 
 window.addEventListener('keydown', keyDown);
 window.addEventListener('keyup', keyUp);
+
+window.onload = init;
